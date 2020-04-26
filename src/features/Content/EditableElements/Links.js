f8991447d73b27df2b8e319b4cdda18a81f6403d
7eppelin <antipvp@gmail.com>
2020-04-26 17:45:31 +0400

    add styles for Content's Links

--- src/features/Content/EditableElements/Links.js
+++ src/features/Content/EditableElements/Links.js
@@ -4,39 +4,64 @@ import styled from 'styled-components/macro'
 import Tooltip from 'components/Tooltip';
 
 const Ul = styled.ul`
+    height: 100%;
+    overflow: hidden;
     
-    li { position: relative; }
+    li { 
+        position: relative;
+        display: inline-block;
+        color: var(--orange3);
+        height: 100%;
+        padding: 11px 50px 0 25px;
+    }
 
     a {
+        display: block;
+        transition: .15s;
+    }
 
+    a:hover {
+        color: var(--orange1);
+    }
+
+    li:hover button {
+        opacity: 1;
+        transform: scale(1);
     }
 
     button {
         position: absolute;
-        top: 3px;
-        right: 3px;
+        top: 12px;
+        right: 23px;
+        background: transparent;
+        font-size: 1.8rem;
+        color: var(--gray3);
         opacity: 0;
-        transform: scale(.8);
-        transition: .3s;
+        transform: scale(.7);
+        transition: .45s;
     }
 
-    &:hover button {
-        opacity: 1;
-        transform: scale(1);
-    }
+    button.close { right: 2px; }
+    button:hover { color: var(--gray1); }
 `;
+
 
 const Links = ({ links, edit }) => (
     <Ul>
         {links.map((link, index) => (
-            <Tooltip tip={link.text} key={index}>
+            <Tooltip tip={link.href} offset={0} key={index}>
                 <li>
                     <a href={link.href} alt={link.href} target='_blank'>
-                    {link.text}
+                        {link.text}
                     </a>
 
                     <button onClick={() => edit(index)}>
-                        e
+                        <i className="fas fa-pen-square"></i>
+                    </button>
+
+                    <button className='close' 
+                        onClick={() => edit(index)}>
+                        <i className="fas fa-times"></i>
                     </button>
                 </li>
             </Tooltip>
