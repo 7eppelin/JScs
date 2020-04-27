2e2851dc1e3da21a38d7ee5fd72e4ff36986df5f
7eppelin <antipvp@gmail.com>
2020-04-27 17:16:28 +0400

    edit components/Scrollbar

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
-// const Thumb = styled.div`
-//     width: 2px !important;
-//     background-color: var(--brown);
-// `;
+// the lib is no longer being maintained
+// probably should find some other solution
+// however, so far it works just fine.
+// https://github.com/malte-wessel/react-custom-scrollbars
 
 const Scrollbar = ({ children }) => (
     <Scrollbars
         autoHide
-        autoHideDuration={200}
+        autoHideTimeout={2000}
+        autoHideDuration={400}
     >
         {children}
     </Scrollbars>
