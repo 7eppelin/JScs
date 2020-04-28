1c0d84c47c4a7dda1de5bc5e1e5e02cecdfa5520
7eppelin <antipvp@gmail.com>
2020-04-28 18:56:24 +0400

    make links block aware of the readOnly mode

--- src/features/Content/EditableElements/LinksElement.js
+++ src/features/Content/EditableElements/LinksElement.js
@@ -3,15 +3,10 @@ import styled from 'styled-components/macro';
 import { motion } from 'framer-motion';
 
 import { Editor } from './../editor';
-import { useSlate } from 'slate-react';
+import { useSlate, useReadOnly } from 'slate-react';
 
 import Links from './Links';
 import LinkForm from './LinkForm';
-
-const elem = {
-    shown: { opacity: 1 },
-    hidden: { opacity: 0 }
-}
 
 
 const LinksContainer = styled(motion.div)`
@@ -25,29 +20,56 @@ const LinksContainer = styled(motion.div)`
     ul {
         padding-right: 40px;
     }
+`;
 
-    & > button {
-        position: absolute;
-        top: 6px;
-        right: 12px;
-        border: 1px solid var(--gray5);
-        background: var(--black);
-        color: var(--gray1);
-        border-radius: 3px;
-        font-size: 1.4rem;
-        padding: 7px 9px;
-        transition: .15s;
-    }
+const AddButton = styled(motion.button)`
+    position: absolute;
+    top: 6px;
+    right: 12px;
+    border: 1px solid var(--gray5);
+    background: var(--black);
+    color: var(--gray1);
+    border-radius: 3px;
+    font-size: 1.4rem;
+    padding: 7px 9px;
+    transition: .15s;
 
-    & > button:hover {
+    &:hover {
         color: var(--green);
     }
 `;
+
+
+const elem = {
+    shown: { opacity: 1 },
+    hidden: { opacity: 0 }
+}
+
+const addButtonVariants = {
+    shown: {
+        opacity: 1,
+        x: 0,
+        scale: 1,
+        transition: {
+            delay: 0.5,
+            type: 'spring',
+            stiffness: 150,
+            damping: 8,
+        }
+    },
+    hidden: {
+        opacity: 0,
+        x: 80,
+        scale: 0.7,
+    }
+}
+
 
 const LinksElement = ({ element, attributes, children }) => {
     const { links } = element;
     const [ editing, setEditing ] = useState(false); // boolean || link index
     const editor = useSlate();
+    const readOnly = useReadOnly();
 
     const deleteLink = useCallback(index => {
         Editor.setLinks(editor,
@@ -61,13 +83,16 @@ const LinksElement = ({ element, attributes, children }) => {
             {...attributes} >
 
             <Links links={links} 
+                readOnly={readOnly}
                 edit={linkIndex => setEditing(linkIndex)}
                 deleteLink={linkIndex => deleteLink(linkIndex)} />
                 
 
-            <button onClick={() => setEditing(true)} >
-                <i className="fas fa-plus"></i>
-            </button>
+            <AddButton variants={addButtonVariants}
+                animate={readOnly ? 'hidden' : 'shown'}
+                onClick={() => setEditing(true)} >
+                    <i className="fas fa-plus"></i>
+            </AddButton>
 
             <LinkForm editing={editing}
                 links={links}
