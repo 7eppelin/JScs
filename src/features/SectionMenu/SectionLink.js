6b6e34243ae48495b78a32d79382eed6b2c452e3
7eppelin <antipvp@gmail.com>
2020-04-30 15:13:05 +0400

    refactor SectionMenu and SectionLink

--- src/features/SectionMenu/SectionLink.js
+++ src/features/SectionMenu/SectionLink.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 
-const StyledLink = styled(NavLink)`
-    padding: 15px;
+const StyledLink = styled(motion.li)`
     display: block;
-    height: 100%;
-    transition: .2s;
-    color: var(--gray1);
+    margin: 2px 0;
     background-color: var(--gray5);
 
-    &:hover {
+    a {
+        display: block;
+        padding: 15px;
+        height: 100%;
+        border-radius: 3px;
+        color: var(--gray1);
+        transition: .2s;
+    }
+
+    a:hover {
         background: var(--gray4);
-        box-shadow: inset 0 0 10px 0 var(--gray6);
         color: var(--white);
     }
 
-    &.active {
-        background-color: var(--gray4);
+    a.active {
+        background: var(--gray4);
         box-shadow: inset 0 0 15px 0 var(--gray6);
         color: var(--white);
     }
@@ -25,9 +31,27 @@ const StyledLink = styled(NavLink)`
 
 
 const SectionLink = ({ label }) => (
-    <StyledLink to={`/${label}`} activeClassName='active'>
-        {label}
+    <StyledLink variants={item}>
+
+        <NavLink to={`/${label}`} activeClassName='active'>
+            {label}
+        </NavLink>
+
     </StyledLink>
 )
+
+
+const item = {
+    hidden: { 
+        opacity: 0,
+        scaleY: 0.6,
+        y: -10
+    },
+    visible: { 
+        opacity: 1,
+        scaleY: 1,
+        y: 0
+    },
+}
 
 export default SectionLink;
--- src/features/SectionMenu/SectionMenu.js
+++ src/features/SectionMenu/SectionMenu.js
@@ -9,23 +9,47 @@ import Scrollbar from 'components/Scrollbar';
 import Spinner from 'components/Spinner';
 import SectionLink from './SectionLink';
 
+
+const SectionMenu = () => {
+    const dispatch = useDispatch();
+    const sectionsObj = useSelector(state => state.data.sections.byID);
+    const sections = Object.values(sectionsObj);
+
+    useEffect(() => {
+        dispatch(getSections());
+    }, [])
+
+    if (!sections.length) return <StyledMenu><Spinner /></StyledMenu>
+
+    return (
+        <StyledMenu>
+            <Scrollbar>
+                <motion.ul variants={list} 
+                    initial='hidden' 
+                    animate='visible' >
+
+                    {sections.map(sec => (
+                        <SectionLink key={sec.id} 
+                            label={sec.name} />
+                    ))}
+
+                </motion.ul>
+            </Scrollbar>
+        </StyledMenu>
+    )
+}
+
+
+
 const StyledMenu = styled.section`
     position: relative;
     background: var(--gray6);
-    padding: 5px;
-    padding-left: 8px;
-    padding-right: 0;
+    padding: 5px 0 5px 8px;
     flex-basis: 160px;
 
     ul {
-        background: var(--gray5);
-        margin-right: 10px;
+        margin-right: 11px;
         height: 100%;
-    }
-
-    li {
-        display: block;
-        border-bottom: 3px solid var(--gray6);
     }
 `;
 
@@ -43,46 +67,5 @@ const list = {
     }
 }
 
-const item = {
-    hidden: { 
-        opacity: 0,
-        scaleY: 0.6,
-        y: -10
-    },
-    visible: { 
-        opacity: 1,
-        scaleY: 1,
-        y: 0
-    },
-}
-
-
-const SectionMenu = () => {
-    const dispatch = useDispatch();
-    const sectionsObj = useSelector(state => state.data.sections.byID);
-    const sections = Object.values(sectionsObj);
-
-    useEffect(() => {
-        dispatch(getSections());
-    }, [])
-
-    if (!sections.length) return <StyledMenu><Spinner /></StyledMenu>
-
-    return (
-        <StyledMenu>
-            <Scrollbar>
-                <motion.ul variants={list} 
-                        initial='hidden' 
-                        animate='visible' >
-                    {sections.map(sec => (
-                        <motion.li key={sec.id} variants={item} >
-                            <SectionLink label={sec.name} />
-                        </motion.li>
-                    ))}
-                </motion.ul>
-            </Scrollbar>
-        </StyledMenu>
-    )
-}
 
 export default SectionMenu;
