6856b40dbbf2365ea0e88e128fe6daf04179c3ce
7eppelin <antipvp@gmail.com>
2020-05-09 19:41:45 +0400

    make SubsectionMenu render subsecs in the right order
    SubsectionMenu renders subsecs in the order in which their id's are in section.children array

--- src/features/SubsectionMenu/SubsectionMenu.js
+++ src/features/SubsectionMenu/SubsectionMenu.js
@@ -12,12 +12,61 @@ import FeatureMenu from './FeatureMenu';
 
 const selectSubsections = createSelector(
     state => state.data.subsections,
-    (_, sectionName) => sectionName,
-    (subs, sectionName) => {
+    (_, secName) => secName,
+    (subs, secName) => {
+        if (!secName) return [];
         const subsArr = Object.values(subs)
-        return subsArr.filter(sub => sub.sectionName === sectionName)
+        return subsArr.filter(sub => sub.sectionName === secName)
     }
 )
+
+const selectIDs = createSelector(
+    state => state.data.sections.byID,
+    (_, secName) => secName,
+    (sections, secName) => {
+        if (!secName) return [];
+        const secsArr = Object.values(sections);
+        const sec = secsArr.filter(sec => sec.name === secName)[0];
+        return sec.children;
+    }
+    
+)
+
+
+const SubsectionMenu = () => {
+    let { sectionName } = useParams();
+    const dispatch = useDispatch();
+
+    // array of subsections
+    const subsecs = useSelector(state => selectSubsections(state, sectionName))
+
+    // array of subsections' ids to render the subsecs in the correct order
+    const ids = useSelector(state => selectIDs(state, sectionName))
+
+    useEffect(() => {
+        // if subsections were fetched before, return
+        if (!sectionName || subsecs.length) return;
+        dispatch(getSubsections(sectionName));
+    }, [sectionName])
+
+    if (!sectionName || !subsecs.length) return (
+        <StyledMenu>
+            <Spinner />
+        </StyledMenu>
+    )
+
+    return (
+        <StyledMenu>
+            <Scrollbar>
+                <ul className='subs'>
+                    {ids.map(id => (
+                        <FeatureMenu key={id} sub={subsecs[id]} />
+                    ))}
+                </ul>
+            </Scrollbar>
+        </StyledMenu>
+    )
+}
 
 
 const StyledMenu = styled.section`
@@ -31,38 +80,5 @@ const StyledMenu = styled.section`
         padding-right: 12px;
     }
 `;
-
-
-const SubsectionMenu = () => {
-    let { sectionName } = useParams();
-    sectionName = sectionName || 'JavaScript';
-    const dispatch = useDispatch();
-
-    const subsections = useSelector(state => selectSubsections(state, sectionName))
-
-    useEffect(() => {
-        // if subsections were fetched before, return
-        if (subsections.length) return;
-        dispatch(getSubsections(sectionName));
-    }, [sectionName])
-
-    if (!subsections.length) return (
-        <StyledMenu>
-            <Spinner />
-        </StyledMenu>
-    )
-
-    return (
-        <StyledMenu>
-            <Scrollbar>
-                <ul className='subs'>
-                    {subsections.map(sub => (
-                        <FeatureMenu key={sub.id} sub={sub} />
-                    ))}
-                </ul>
-            </Scrollbar>
-        </StyledMenu>
-    )
-}
 
 export default SubsectionMenu;
