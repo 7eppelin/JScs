0ded2d2fb44f0fb0022d6cfbadb246c6d7618b75
7eppelin <antipvp@gmail.com>
2020-05-13 17:19:21 +0400

    move slice into slice.js and add re-exports to index

---
+++ src/dataSlice/index.js
@@ -0,0 +1,17 @@
+import reducer from './slice'
+export default dataReducer
+
+export { 
+    addSections,
+    addSubsections,
+    addFeatures, 
+    addNewSubsection,
+    addNewFeature,
+    removeSection,
+    removeSubsection,
+    removeFeature,
+    reorderSections,
+    reorderSubsections,
+    reorderFeatures,
+    addContentItem 
+} from './slice'
