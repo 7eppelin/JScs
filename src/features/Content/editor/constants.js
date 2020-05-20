29901c82d9858828656f87056cf13f6aa660e555
7eppelin <antipvp@gmail.com>
2020-05-20 12:15:03 +0400

    add marks to editor/constants

--- src/features/Content/editor/constants.js
+++ src/features/Content/editor/constants.js
@@ -1,3 +1,4 @@
+import React from 'react'
 
 export const ELEMS = [
     'paragraph',
@@ -7,12 +8,23 @@ export const ELEMS = [
     'code-block'
 ]
 
+export const MARKS = [
+    'bold',
+    'italic',
+    'code',
+    'link',
+    'tooltip'
+]
+
 export const HOTKEYS = {
+    'code-block': 'ctrl + h',
     'paragraph': 'ctrl + p',
     'h2': 'ctrl + 2',
     'h3': 'ctrl + 3',
     'ul': 'ctrl + u',
-    'code-block': 'ctrl + h'
+    'bold': 'ctrl + b',
+    'italic': 'ctrl + i',
+    'code': 'ctrl + `'
 }
 
 export const ICONS = {
@@ -21,4 +33,9 @@ export const ICONS = {
     h2: 'H2',
     h3: 'H3',
     ul: 'UL',
+    bold: 'B',
+    italic: 'I',
+    code: '</>',
+    link: <i className="fas fa-link" />,
+    tooltip: <i className="far fa-comment-alt" />
 }
