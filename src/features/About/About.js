ee9242ff8bc3e940183589e39f11c46ffb27b89a
7eppelin <antipvp@gmail.com>
2020-05-26 14:53:25 +0400

    basic About section animation
    most likely, it's going to be modified in future

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import { motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 
 
 const About = () => {
+    const location = useLocation()
+    const url = location.pathname
+    const animate = (url === '/') ? 'enter' : 'exit'
+
     return (
-        <Section>
-            THIS IS THE FRONT PAGE OF THE APP
+        <Section variants={variants}
+            initial='initial'
+            animate={animate}>
+
+                THIS IS THE FRONT PAGE OF THE APP
         </Section>
     )
 }
 
 const Section = styled(motion.section)`
     background: var(--gray6);
-    flex-basis: 600;
+    flex-basis: 50vw;
     flex-grow: 1;
-    margin: 0 30px;
+    margin: 0 4vw;
     box-shadow: 0 0 30px -5px black;
     border-radius: 5px;
 `
+
+
+const transition = {
+    type: 'spring',
+}
+
+
+const variants = {
+    initial: {
+        opacity: 0,
+        scale: .9,
+        x: -1000,
+    },
+
+    exit: {
+        opacity: [ 1, .8, 0 ],
+        scale: [ 1, .9, .9 ],
+        x: [ 0, 0, -1000 ],
+        transition
+    },
+
+    enter: {
+        opacity: [ 0, .8, 1 ],
+        scale: [ .9, .9, 1 ],
+        x: [ -1000, 0, 0 ],
+        transition
+    }
+}
 
 export default About
