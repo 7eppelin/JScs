3354e7d0563994a2d744072f862a325042009410
7eppelin <antipvp@gmail.com>
2020-06-06 13:46:39 +0400

    edit AddFormContainer

--- src/features/AddForm/AddFormContainer.js
+++ src/features/AddForm/AddFormContainer.js
@@ -5,21 +5,21 @@ import AddFormToggler from './AddFormToggler.js';
 import AnimateAddForm from './AnimateAddForm.js';
 
 
-const StyledContainer = styled.div`
-    position: relative;
-    display: inline-block;
-    margin-left: 45px;
-`;
-
 const AddFormContainer = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <StyledContainer>
+        <Container>
             <AddFormToggler toggle={setIsOpen} isToggled={isOpen} />
             <AnimateAddForm isOpen={isOpen} hide={() => setIsOpen(false)} />
-        </StyledContainer>
+        </Container>
     )
-};
+}
+
+const Container = styled.div`
+    position: relative;
+    display: inline-block;
+    margin-left: 45px;
+`;
 
 export default AddFormContainer;
