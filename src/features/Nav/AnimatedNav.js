1393b162d27e4793cd4e9eb1e944d359b78c78a3
7eppelin <antipvp@gmail.com>
2020-06-19 13:15:47 +0400

    extract styles and animation from Nav  into AnimatedNav

---
+++ src/features/Nav/AnimatedNav.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { motion } from 'framer-motion'
+
+
+const AnimatedNav = ({ activeSection, children }) => {
+    return (
+        <StyledNav variants={variants}
+            initial={false}
+            animate={activeSection ? 'content' : 'about'}>
+
+                {children}
+        </StyledNav>
+    )
+}
+
+
+const StyledNav = styled(motion.nav)`
+    position: absolute;
+    box-shadow: 0 0 30px -5px black;
+    background: var(--gray6);
+    width: 32vw;
+    height: calc(100% - 50px);
+    top: 25px;
+    margin-right: 30px;
+    display: flex;
+`;
+
+const transition = {
+    left: {
+        delay: 0.48,
+        type: 'spring',
+        stiffness: 110,
+        damping: 14,
+        mass: 0.9,
+    }
+}
+
+const variants = {
+    content: {
+        left: '0vw',
+        scaleY: 1,
+        transition
+    },
+    about: {
+        left: '86vw',
+        scaleY: 0.9,
+        transition
+    }
+}
+
+
+export default AnimatedNav
--- src/features/Nav/Nav.js
+++ src/features/Nav/Nav.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styled from 'styled-components/macro';
-import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
 
 import { usePrevious } from 'utils'
 import useSections from './useSections'
 
+import AnimatedNav from './AnimatedNav'
 import Spinner from 'components/Spinner'
 import SectionMenu from 'features/SectionMenu/SectionMenu';
 import SubsecsMenu from 'features/SubsecsMenu/SubsecsMenu';
@@ -20,10 +19,7 @@ const Nav = ({ activeSection, delayAnimation }) => {
     const prevSection = usePrevious(activeSection)
 
     return (
-        <StyledNav variants={variants}
-            initial={false}
-            animate={activeSection ? 'content' : 'about'}>
-
+        <AnimatedNav activeSection={activeSection}>
             {/* 
                 only render spinner in the nav when:
                 1. this is the initial render of the app
@@ -46,44 +42,8 @@ const Nav = ({ activeSection, delayAnimation }) => {
                         delayAnimation={delayAnimation} />
                 </>
             }
-
-        </StyledNav>
+        </AnimatedNav>
     )
-}
-
-
-const StyledNav = styled(motion.nav)`
-    position: absolute;
-    box-shadow: 0 0 30px -5px black;
-    background: var(--gray6);
-    width: 32vw;
-    height: calc(100% - 50px);
-    top: 25px;
-    margin-right: 30px;
-    display: flex;
-`;
-
-const transition = {
-    left: {
-        delay: 0.48,
-        type: 'spring',
-        stiffness: 110,
-        damping: 14,
-        mass: 0.9,
-    }
-}
-
-const variants = {
-    content: {
-        left: '0vw',
-        scaleY: 1,
-        transition
-    },
-    about: {
-        left: '86vw',
-        scaleY: 0.9,
-        transition
-    }
 }
 
 export default Nav;
